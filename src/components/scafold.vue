5e21e47a3f7bc5f45bd845dbdff53fb45c9bde50
Takumi Washio <taku_washio@ezweb.ne.jp>
2020-09-16 10:27:31 +0900

    I made sidebar

---
+++ src/components/scafold.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <top></top>
+    <sidebar class="sidebar-area"></sidebar>
+    <div class="scafold-wrapper text-center">
+    <h1 id = "aaa">aaa</h1>
+    <h1 id = "bbb">bbb</h1>
+      <router-view/>
+    </div>
+    <h1 id = "ccc">ccc</h1>
+    <h1 id = "ddd">ddd</h1>
+  </div>
+</template>
+
+<script>
+import Sidebar from './sidebar.vue' // サイドバー
+export default {
+  components: {
+    Sidebar
+  },watch: {
+    '$route': function (n, o) {
+      if (n.hash.match(/^#/)) {
+        document.getElementById(n.hash.replace(/^#/, '')).scrollIntoView()
+      }
+      console.log('new, old', [n.hash, o.hash])
+    }
+  },
+  mounted () {
+    if (this.$route.hash.match(/^#/)) {
+      document.getElementById(this.$route.hash.replace(/^#/, '')).scrollIntoView()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sidebar-area {
+  /* 左側に固定 */
+  float: left;
+}
+
+
+.scafold-wrapper {
+  /* display: flex; 要素を横並びにする */
+  flex-direction: column; /* 要素の並び順の主軸を指定 上 => 下 */
+  min-height: 100vh; /* 要素の高さの最小値を指定 vhはviewport(表示領域) heightの略 */
+
+  /* サイドバーのwidth分だけ範囲を削除 */
+  width: calc(100% - 200px);
+
+  /* サイドバーで隠れるので右に寄せる */
+  margin: 0 0 0 180px;
+}
+</style>
---
+++ src/components/sidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-wrapper">
+        <!-- サイドバーメニュー -->
+        <p><router-link :to="{name:'scafold',hash:'#aaa'}" class="sidebar-link">aaa</router-link></p>
+        <p><router-link :to="{name:'scafold',hash:'#bbb'}" class="sidebar-link">bbb</router-link></p>
+        <p><router-link :to="{name:'scafold',hash:'#ccc'}" class="sidebar-link">ccc</router-link></p>
+        <p><router-link :to="{name:'scafold',hash:'#ddd'}" class="sidebar-link">ddd</router-link></p>
+    </div>
+  </div>
+</template>
+
+<style>
+.sidebar {
+  background-color: #bf0000;
+  height: 100%; /* サイドバーの高さ */
+  width: 200px; /* サイドバーのwidthを指定 */
+  max-width: 200px; /* widthの最大値 */
+  opacity: 0.9.5; /* 透過する 0に近くほど透過する */
+  position: fixed; /* 左上に要素を固定する(スクロールしても位置は固定される) */
+  overflow-x: hidden; /* 横軸ではみ出た要素を非表示にする */
+  box-sizing: border-box; /* paddingとborderを、widthとheightに含める */
+  padding-left: 40px; /* サイドバー内のリンクの位置を右にずらす */
+}
+
+
+
+.sidebar-link {
+  color: #ffffff; /* リンクの文字色を白に */
+}
+
+.sidebar-link:hover {
+  color: #ffffff /* マウスがリンクに乗った時も文字色を白に */
+}
+</style>
